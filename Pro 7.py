605d40c882fcd1ad6edfc2eb2a677c90448c0f44
Harsha S <157780689+harshasnayaka@users.noreply.github.com>
2024-01-30 21:30:55 +0530

    Add files via upload

---
+++ Pro 7.py
@@ -0,0 +1,14 @@
+print("A small code about OTP : ")
+name = input("Enter your Name : ")
+if name == "Harsha":
+    print("You are Welcome !")
+else :
+    print("Hi , Stranger!")
+contact = int(input("Enter your Contact Number : "))
+print("OTP send Succesfully")
+print(" Duration : 5 min ")
+otp = int(input("Enter OTP : "))
+if otp == 5757:
+    print("Welcome!")
+else:
+    print("Invalid OTP!")
